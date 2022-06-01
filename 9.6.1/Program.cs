dbd40c0cf6da66754fe8d94f514660c022cf0a8c
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-02 03:27:42 +0600

    V1.?

--- 9.6.1/Program.cs
+++ 9.6.1/Program.cs
@@ -4,22 +4,57 @@ namespace _9._6._1
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exception[] exceptions = new Exception[5];
+            Exception exp1 = new ArgumentException("Недопустимый аргумент");
+            
+ 
+
+
+            try
+            {
+              int result =  Div(4,4);
+                Console.WriteLine(result);
+                foreach (Exception ex in exceptions)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            catch(ArgumentException)
+            {
+                Console.WriteLine(" ");  
+            }
+            catch (MyException)
+            {
+                Console.WriteLine("Делить на ноль нельзя");
+            }
+
+            catch (TimeoutException)
+            {
+                Console.WriteLine(" ");
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine(" ");
+            }
+            
+
+            static int Div(int a,int b)
+            {
+                return a / b;
+            }
         }
     }
 
-    class MyException : Exception
+    class MyException : DivideByZeroException
     {
-        public MyException()
-        { 
-        
-        }
+        public MyException(string message) : base(message)
+        {
 
-        public MyException(string message): base(message)
-        { 
-        
         }
     }
 }
