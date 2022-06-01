ef089d101633c921fced4fbbbe88786c66f6466e
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-02 02:37:30 +0600

    Добавьте файлы проекта.

---
+++ 9.6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _9._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class MyException : Exception
+    {
+        public MyException()
+        { 
+        
+        }
+
+        public MyException(string message): base(message)
+        { 
+        
+        }
+    }
+}
